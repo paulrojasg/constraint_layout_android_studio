f967b651909289334b3ced44399b63856ecef8ba
paulrojasg <paul.rojas@correounivalle.edu.co>
2024-03-14 07:09:09 -0500

    First commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "constraintlayout_class4"
+include(":app")
+ 
